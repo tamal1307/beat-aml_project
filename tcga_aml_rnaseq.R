7591927cc41af7dd742111e32c6c5b9c84fe5d6c
tamal_gandu <tamal1307@gmail.com>
2021-08-06 09:20:48 +0530

    editted aug 6

--- tcga_aml_rnaseq.R
+++ tcga_aml_rnaseq.R
@@ -1,4 +1,3 @@
-kuch vbi likh
 #setting working directory
 setwd("A:/kiran/tcga_laml_rnaseq/tcga_laml_rnaseq/")
 #load("A:/kiran/tcga_laml_rnaseq/tcga_laml_rnaseq/renv.RData")
@@ -13,10 +12,10 @@ y=read.table('tcga_aml_rnaseq.txt', header=T, row.names=1, sep='\t')
 #library(org.Hs.eg.db)
 
 # 1. Convert from ensembl.gene to gene.symbol
-ent_ids= rownames(y)
-geneIDs1 <- ensembldb::select(EnsDb.Hsapiens.v79, 
-keys= cat(a), keytype = "GENEID", 
-columns = c("SYMBOL","GENEID"))
+#ent_ids= rownames(y)
+#geneIDs1 <- ensembldb::select(EnsDb.Hsapiens.v79, 
+#keys= cat(a), keytype = "GENEID", 
+#columns = c("SYMBOL","GENEID"))
 
 head(ent_ids)
 #library("org.Hs.eg.db") # remember to install it if you don't have it already
@@ -31,20 +30,20 @@ head(x)
 #opening folders and readinf files
 #https://stackoverflow.com/questions/51545792/read-specific-csv-files-in-multiple-directories-with-r?rq=1
 
-#file_list <- list.files(path="A:/kiran/tcga_laml_rnaseq/tcga_laml_rnaseq/",
-#                        recursive = T, pattern = "\\.txt.gz$" )
+file_list <- list.files(path="A:/kiran/tcga_laml_rnaseq/tcga_laml_rnaseq/",
+                        recursive = T, pattern = "\\.txt.gz$" )
 #$ at the end means that this is end of string. "dbf$" will work too,
 # but adding \\.(. is special character in regular expressions so you 
 #need to escape it) ensure that you match only files with extension 
 #.dbf (in case you have e.g. .adbf files).
 
-#data_lst = lapply(file_list,read.table, row.names=1)
+data_lst = lapply(file_list,read.table, row.names=1)
 
-#y= as.data.frame(data_lst[1], stringsAsFactors = T)
+y= as.data.frame(data_lst[1], stringsAsFactors = T)
 
-#for (i in 2:length(data_lst)){
-#  y$i= cbind.data.frame(y[,-1],as.data.frame(data_lst[i], stringsAsFactors = T))
-#}
+for (i in 2:length(data_lst)){
+  y$i= cbind.data.frame(y[,-1],as.data.frame(data_lst[i], stringsAsFactors = T))
+}
 #write.table(y, 'tcga_aml_rnaseq.txt', sep='\t')
 
 #write.table(ent_ids, 'gene_ids.txt', sep='\t')
